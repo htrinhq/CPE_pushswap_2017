dc76196427eef823e7295c5958b5a1b5a0639a03
Hugo TRINH QUY <hquy@localhost.localdomain>
2017-11-24 18:06:36 +0100

    change main

--- source/main.c
+++ source/main.c
@@ -17,9 +17,10 @@ int main(int ac, char **av)
 
 	if (ac < 2)
 		return (84);
-	if (ac > 1000)
+	else if (ac > 1001) {
+		write(1, "\n", 1);
 		return (0);
-	else {
+	} else {
 		src = fillsrc(src, ac, av);
 		pushswap(src, lb, &b, size);
 		return (0);
